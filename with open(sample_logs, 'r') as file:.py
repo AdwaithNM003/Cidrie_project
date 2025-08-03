fe454c9730dba6774a503f3bfdf59d01a0740be3
AdwaithNM003 <adwaithnm003@gmail.com>
2025-08-04 00:30:24 +0530

    Delete with open(sample_logs, 'r') as file:.py

--- with open(sample_logs, 'r') as file:.py
+++
@@ -1,6 +0,0 @@
-with open("sample_logs.txt", 'r') as file:
-        for line in file:
-            if "ERROR" in line:
-                time = line.split(']')[0].strip('[')
-                ermsg= line.split('ERROR:')[1].strip()
-                print(f"Timestamp: {time}, Error Message: {ermsg}")
